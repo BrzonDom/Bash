cda4e62a9e18a79a533737c2100c9dc4cdd447fd
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-25 20:28:59 +0200

    Bash 01.Tut v01.3

--- Tutorial/freeCodeCamp/01.02.HelloWorld.sh
+++ Tutorial/freeCodeCamp/01.02.HelloWorld.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
-#Use cat command to print content of a text file
+# echo syntax:
+#   echo [option] [string]
+#   The echo command in Linux displays a string provided by the user.
+
+# Use cat command to print content of a text file
 
 cat Resources/01.HelloWorldTxt.txt
