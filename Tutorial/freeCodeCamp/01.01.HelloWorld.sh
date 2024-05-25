b6379acd0334e621feac0a5acee6b91ec4314f9f
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-25 20:28:48 +0200

    Bash 01.Tut v01.3

--- Tutorial/freeCodeCamp/01.01.HelloWorld.sh
+++ Tutorial/freeCodeCamp/01.01.HelloWorld.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
-#Use echo command to print "Hello World"
+# echo syntax:
+#   echo [option] [string]
+#   The echo command in Linux displays a string provided by the user.
+
+# Use echo command to print "Hello World"
 
 echo "Hello World"
