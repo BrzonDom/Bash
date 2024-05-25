21933319d74bb0d7c402563e0f07252fa4709d99
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-25 20:34:03 +0200

    Bash 02.Tut v02.3

--- Tutorial/freeCodeCamp/02.01.Variables.sh
+++ Tutorial/freeCodeCamp/02.01.Variables.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 
-#Use variable=value to print my full name 
+# Variavble syntax:
+#   Variable=<value>
+#   Variables are the containers which store data or a useful piece of information as the value inside them
+
+# Use Variable=value to print my full name 
 
 First_Name=Dominik
 Second_Name=Brzon
