7eda356bce24034f9eca6167a601b23a14bc0aa0
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-25 20:36:59 +0200

    Bash 03.Tut v02.2

--- Tutorial/freeCodeCamp/03.01.PositionArgument.sh
+++ Tutorial/freeCodeCamp/03.01.PositionArgument.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
+# read syntax:
+#   read <options> <arguments>
+#   The read command takes the user input and splits the string into fields, assigning each new word to an argument. 
+#   If there are fewer variables than words, read stores the remaining terms into the final variable.  
+
+# Use read to read user input
+
 echo What is your first name ?
 read First_Name
 
