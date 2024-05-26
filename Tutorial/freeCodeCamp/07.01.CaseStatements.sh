cee9b9405e477657e52b409a2a24644b04ff824b
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-26 12:29:56 +0200

    Bash 07.Tut v02.0

--- Tutorial/freeCodeCamp/07.01.CaseStatements.sh
+++ Tutorial/freeCodeCamp/07.01.CaseStatements.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/bash
 
+# case - Execute commands based on pattern matching.
+# SYNOPSIS:
+#   case WORD in [PATTERN [| PATTERN]...) COMMANDS ;;]... esac
+# DESCRIPTION:
+#    Execute commands based on pattern matching.
+#    
+#    Selectively execute COMMANDS based upon WORD matching PATTERN.  The
+#    '|' is used to separate multiple patterns.
+#    
+#    Exit Status:
+#    Returns the status of the last command executed.
+
+
 case ${1,,} in
     milan | administrator)
         echo "Hello admin";;
