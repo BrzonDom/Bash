39abc5069d1901284c7047bc8258a945c1b4b3a1
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-26 12:34:27 +0200

    Bash 06.Tut v02.0

--- Tutorial/freeCodeCamp/06.01.IfElifElse.sh
+++ Tutorial/freeCodeCamp/06.01.IfElifElse.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/bash
 
+# if - Execute commands based on conditional.
+# SYNOPSIS:
+#   if COMMANDS; then COMMANDS; [ elif COMMANDS; then COMMANDS; ]... [ else COMMANDS; ] fi
+# DESCRIPTION
+#   Execute commands based on conditional.
+#
+#   The "if COMMANDS" list is executed.  If its exit status is zero, then the
+#   "then COMMANDS" list is executed.  Otherwise, each "elif COMMANDS" list is
+#   executed in turn, and if its exit status is zero, the corresponding
+#   "then COMMANDS" list is executed and the if command completes.  Otherwise,
+#   the "else COMMANDS" list is executed, if present.  The exit status of the
+#   entire construct is the exit status of the last command executed, or zero
+#   if no condition tested true.
+#    
+#   Exit Status:
+#      Returns the status of the last command executed.
+
+
 if [ ${1,,} = dominik ]; then
     echo "Hello Dominik"
 
