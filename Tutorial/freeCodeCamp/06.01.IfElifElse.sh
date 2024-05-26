5b42c003366ddb8ca017bfd59e7e9beafd264809
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-26 12:37:40 +0200

    Bash 06.Tut v02.1

--- Tutorial/freeCodeCamp/06.01.IfElifElse.sh
+++ Tutorial/freeCodeCamp/06.01.IfElifElse.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/bash
 
 # if - Execute commands based on conditional.
-# SYNOPSIS:
-#   if COMMANDS; then COMMANDS; [ elif COMMANDS; then COMMANDS; ]... [ else COMMANDS; ] fi
-# DESCRIPTION
-#   Execute commands based on conditional.
+#   SYNOPSIS:
+#       if COMMANDS; then COMMANDS; [ elif COMMANDS; then COMMANDS; ]... [ else COMMANDS; ] fi
+#   DESCRIPTION
+#       Execute commands based on conditional.
 #
-#   The "if COMMANDS" list is executed.  If its exit status is zero, then the
-#   "then COMMANDS" list is executed.  Otherwise, each "elif COMMANDS" list is
-#   executed in turn, and if its exit status is zero, the corresponding
-#   "then COMMANDS" list is executed and the if command completes.  Otherwise,
-#   the "else COMMANDS" list is executed, if present.  The exit status of the
-#   entire construct is the exit status of the last command executed, or zero
-#   if no condition tested true.
+#       The "if COMMANDS" list is executed.  If its exit status is zero, then the
+#       "then COMMANDS" list is executed.  Otherwise, each "elif COMMANDS" list is
+#       executed in turn, and if its exit status is zero, the corresponding
+#       "then COMMANDS" list is executed and the if command completes.  Otherwise,
+#       the "else COMMANDS" list is executed, if present.  The exit status of the
+#       entire construct is the exit status of the last command executed, or zero
+#       if no condition tested true.
 #    
-#   Exit Status:
-#      Returns the status of the last command executed.
+#       Exit Status:
+#           Returns the status of the last command executed.
 
 
 if [ ${1,,} = dominik ]; then
@@ -28,3 +28,6 @@ else
     echo "I don't know who you are"
 
 fi
+
+# Execute:
+#   bash 07.01.CaseStatements.sh [Username]
