708f3380f1683ac74251ce998b0330f3cdca4adc
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-26 12:41:31 +0200

    Bash 07.Tut v02.1

--- Tutorial/freeCodeCamp/07.01.CaseStatements.sh
+++ Tutorial/freeCodeCamp/07.01.CaseStatements.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 
 # case - Execute commands based on pattern matching.
-# SYNOPSIS:
-#   case WORD in [PATTERN [| PATTERN]...) COMMANDS ;;]... esac
-# DESCRIPTION:
-#    Execute commands based on pattern matching.
+#   SYNOPSIS:
+#       case WORD in [PATTERN [| PATTERN]...) COMMANDS ;;]... esac
+#   DESCRIPTION:
+#       Execute commands based on pattern matching.
 #    
-#    Selectively execute COMMANDS based upon WORD matching PATTERN.  The
-#    '|' is used to separate multiple patterns.
+#       Selectively execute COMMANDS based upon WORD matching PATTERN.  The
+#       '|' is used to separate multiple patterns.
 #    
-#    Exit Status:
-#    Returns the status of the last command executed.
+#       Exit Status:
+#           Returns the status of the last command executed.
 
 
 case ${1,,} in
@@ -23,3 +23,6 @@ case ${1,,} in
     *)
         echo "You are not the admin"
 esac
+
+# Execute:
+#   bash 07.01.CaseStatements.sh [Username]
