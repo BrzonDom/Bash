6941cb030050b9e22b77902c003e89dc9f2ef925
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-26 02:17:09 +0200

    Bash 01.Tut v01.5

--- Tutorial/freeCodeCamp/01.02.HelloWorld.sh
+++ Tutorial/freeCodeCamp/01.02.HelloWorld.sh
@@ -7,6 +7,13 @@
 #   DESCRIPTION:
 #       Echo the STRING(s) to standard output.
 
+# cat - concatenate files and print on the standard output
+#   SYNOPSIS:
+#       cat [OPTION]... [FILE]...
+#   DESCRIPTION:
+#       Concatenate FILE(s) to standard output.
+#       With no FILE, or when FILE is -, read standard input.
+
 # Use cat command to print content of a text file
 
 cat Resources/01.HelloWorldTxt.txt
