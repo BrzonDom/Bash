c65d5fdceaa794e3ce4d0af61390eb899968fe0f
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-26 02:20:35 +0200

    Bash 03.Tut v02.3

--- Tutorial/freeCodeCamp/03.01.PositionArgument.sh
+++ Tutorial/freeCodeCamp/03.01.PositionArgument.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/bash
 
-# read syntax:
-#   read <options> <arguments>
-#   The read command takes the user input and splits the string into fields, assigning each new word to an argument. 
-#   If there are fewer variables than words, read stores the remaining terms into the final variable.  
+# read - Read a line from the standard input and split it into fields.
+#   SYNOPSIS:
+#       read [-ers] [-a array] [-d delim] [-i text] [-n nchars] [-N nchars] [-p prompt] [-t timeout] [-u fd] [name ...]
+#   DESCRIPTION:
+#       Read a line from the standard input and split it into fields.
+#
+#       Reads a single line from the standard input, or from file descriptor FD
+#       if the -u option is supplied.  The line is split into fields as with word
+#       splitting, and the first word is assigned to the first NAME, the second
+#       word to the second NAME, and so on, with any leftover words assigned to
+#       the last NAME.  Only the characters found in $IFS are recognized as word
+#       delimiters.
+#
+#       If no NAMEs are supplied, the line read is stored in the REPLY variable.
 
 # Use read to read user input
 
