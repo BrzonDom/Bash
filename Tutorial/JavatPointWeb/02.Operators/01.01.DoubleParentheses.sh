01d6da12f14df0c535e02170daf7b1c30f6949ff
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 17:48:40 +0200

    Bash 02.Tut 1 v02.2

--- Tutorial/JavatPointWeb/02.Operators/01.01.DoubleParentheses.sh
+++ Tutorial/JavatPointWeb/02.Operators/01.01.DoubleParentheses.sh
@@ -3,14 +3,18 @@
 Num1=10
 Num2=3
 
+echo "1.Method:"
+echo -e "    ((Sum=Num1+Num2))\n"
 
 ((Sum=Num1+Num2))
 
-echo "Sum = $Sum"
+echo -e "\tSum = $Sum\n"
 
 unset Sum
 
+echo "2.Method:"
+echo -e "    Sum=$((Num1+Num2))\n"
 
 Sum=$((Num1+Num2))
 
-echo "Sum = $Sum"
+echo -e "\tSum = $Sum"
