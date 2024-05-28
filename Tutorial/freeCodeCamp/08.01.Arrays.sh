dc0df9cccafc0a75e0e01371e285de5995044b21
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 11:06:14 +0200

    Bash 08.Tut v02.2

---
+++ Tutorial/freeCodeCamp/08.01.Arrays.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+FIRST_LIST=(one two three four five)
+
+echo Full List: ${FIRST_LIST[@]}
+echo 
+
+echo 1.Item: $FIRST_LIST
+echo 2.Item: ${FIRST_LIST[1]}
+echo 3.Item: ${FIRST_LIST[2]}
+
+# Execute:
+#   bash 08.01.Arrays.sh
