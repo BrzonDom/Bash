453fde50532525f61f2fb9e4a2a6391ed1774801
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 18:19:54 +0200

    Bash 02.Tut 2 v02.0

--- Tutorial/JavatPointWeb/02.Operators/01.02.DoubleParentheses.sh
+++ Tutorial/JavatPointWeb/02.Operators/01.02.DoubleParentheses.sh
@@ -4,22 +4,22 @@ Num1=8
 Num2=2
 
 echo "Addition:"
-echo -e "\t" $(($Num1 + $Num2)) "\n"
+echo -e "\t$Num1 + $Num2 = $(($Num1 + $Num2))\n"
 
 echo "Subtraction:"
-echo -e "\t" $(($Num1 - $Num2)) "\n"
+echo -e "\t$Num1 - $Num2 = $(($Num1 - $Num2))\n"
 
 echo "Multiplication:"
-echo -e "\t" $(($Num1 * $Num2)) "\n"
+echo -e "\t$Num1 * $Num2 = $(($Num1 * $Num2))\n"
 
 echo "Division:"
-echo -e "\t" $(($Num1 / $Num2)) "\n"
+echo -e "\t$Num1 / $Num2 = $(($Num1 / $Num2))\n"
 
 echo "Exponentiation:"
-echo -e "\t" $(($Num1 ** $Num2)) "\n"
+echo -e "\t$Num1 ** $Num2 = $(($Num1 ** $Num2))\n"
 
 echo "Modular Division:"
-echo -e "\t" $(($Num1 % $Num2)) "\n"
+echo -e "\t$Num1 % $Num2 = $(($Num1 % $Num2))\n"
 
 
 ((Num1 += 5))
