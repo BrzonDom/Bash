20810bebee5142dc3a355f2c2178d3a4ef7bbdbd
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 19:10:38 +0200

    Bash 03.Tut 1 v01.1

--- Tutorial/JavatPointWeb/03.Conditional_Statements/01.01.BashIf.sh
+++ Tutorial/JavatPointWeb/03.Conditional_Statements/01.01.BashIf.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Basic if Statements
+
+# A basic if statement commands that if a particular condition is true, 
+# then only execute a given set of actions. If it is not true, then do 
+# not execute those actions. If statement is based on the following 
+# format:
+
+# if [ expression ];
+#   then
+#       statements
+# fi
+
+
 if [ true ]; then
     echo "Condition is True"
 
