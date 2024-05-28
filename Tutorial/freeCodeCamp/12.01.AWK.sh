d194521c5e3ff67d4a8f701193f80758e2411c81
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 16:11:08 +0200

    Bash 12.Tut v03.0

--- Tutorial/freeCodeCamp/12.01.AWK.sh
+++ Tutorial/freeCodeCamp/12.01.AWK.sh
@@ -5,11 +5,11 @@ CSV_File=Resources/12.01.AWKContentCSV.csv
 
 echo one two three > $TXT_File
 
-awk '{print $1}' $TXT_File
-awk '{print $2}' $TXT_File
+awk -F ' ' '{print $1}' $TXT_File
+awk -F ' ' '{print $2}' $TXT_File
 
 
 echo one,two,three > $CSV_File
 
-awk -F, '{print $1}' $CSV_File
-awk -F, '{print $2}' $CSV_File
+awk -F ',' '{print $1}' $CSV_File
+awk -F ',' '{print $2}' $CSV_File
