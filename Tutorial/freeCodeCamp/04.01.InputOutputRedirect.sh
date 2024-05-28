e38a2da447ad8ef754c44ea16297d944bb9f613d
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 13:32:08 +0200

    Bash 04.Tut v02.2

--- Tutorial/freeCodeCamp/04.01.InputOutputRedirect.sh
+++ Tutorial/freeCodeCamp/04.01.InputOutputRedirect.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+# Use positional arguments to print first and last name
 
 echo Hello $First_Name $Last_Name
 
 # Execute:
-#   bash 04.01.InputOutputRedirect.sh First_Name Last_Name
+#   bash 04.01.InputOutputRedirect.sh [First_Name] [Last_Name]
