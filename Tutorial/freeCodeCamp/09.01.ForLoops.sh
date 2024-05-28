f01f02c32ffc78b97fac09bd678f2de952b59f2f
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 13:08:47 +0200

    Bash 09.Tut v02.3

--- Tutorial/freeCodeCamp/09.01.ForLoops.sh
+++ Tutorial/freeCodeCamp/09.01.ForLoops.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/bash
 
+# For loop
+#   SYNOPSIS:
+#       for element in [list]
+#       do
+#	        [commands]
+#       done
+#   DESCRIPTION:
+#       The for loop is an essential programming functionality that goes 
+#       through a list of elements. For each of those elements, the for 
+#       loop performs a set of commands. The command helps repeat processes 
+#       until a terminating condition.
+
+#       Whether you're going through an array of numbers or renaming files, 
+#       for loops in Bash scripts provide a convenient way to list items 
+#       automatically.
+
+
 StrNum_List=(one two three four five)
 
 for item in ${StrNum_List[@]};
