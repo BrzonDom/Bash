eddfd952bdda78059dd3048cf0d19820fbbb2964
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 17:55:32 +0200

    Bash 02.Tut 2 v01.2

--- Tutorial/JavatPointWeb/02.Operators/01.02.DoubleParentheses.sh
+++ Tutorial/JavatPointWeb/02.Operators/01.02.DoubleParentheses.sh
@@ -7,4 +7,7 @@ echo "Addition:"
 echo -e "\t" $(($Num1 + $Num2)) "\n"
 
 echo "Subtraction:"
-echo -e "\t" $(($Num1 - $Num2))
+echo -e "\t" $(($Num1 - $Num2)) "\n"
+
+echo "Multiplication:"
+echo -e "\t" $(($Num1 * $Num2))
