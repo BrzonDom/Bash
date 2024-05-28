933c022440af7245fb733da18d375cc080ec8131
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 17:11:39 +0200

    Bash 02.Tut 1 v02.0

--- Tutorial/JavatPointWeb/02.Operators/01.01.DoubleParentheses.sh
+++ Tutorial/JavatPointWeb/02.Operators/01.01.DoubleParentheses.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 Sum=$((10+3))
+
 echo "Sum = $Sum"
 
+unset Sum
+
+
 ((Sum=10+3))
+
 echo "Sum = $Sum"
+
+unset Sum
 
 Num1=10
 Num2=3
 ((Sum=Num1+Num2))
+
 echo "Sum = $Sum"
+
+unset Num1 Num2 Sum
 
 Num1=10  
 Num2=3  
-Sum=$((Num1+Num2))  
-echo "Sum = $Sum"  
+Sum=$((Num1+Num2))
+
+echo "Sum = $Sum"
