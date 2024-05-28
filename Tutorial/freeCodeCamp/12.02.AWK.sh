fc06bd91d61ba6de65d570dffb8f2bf11d3b92d5
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 16:31:35 +0200

    Bash 12.Tut v02.0

--- Tutorial/freeCodeCamp/12.02.AWK.sh
+++ Tutorial/freeCodeCamp/12.02.AWK.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 
 echo "Just get this word: Hello" | awk '{print $5}'
 
 echo "Just get this word: Hello" | awk -F: '{print $2}' | cut -c2
 
-echo "Just get this word: Hello" | awk -F: '{print $2}' | cut -c2-
+echo "Just get this word: Hello" | awk -F: '{print $2}' | cut -c3-
