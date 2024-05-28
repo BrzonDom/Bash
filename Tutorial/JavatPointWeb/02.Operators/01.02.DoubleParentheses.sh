f4eff1413edc423e0a0a60972b7f84767e4a5a20
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 18:05:03 +0200

    Bash 02.Tut 2 v01.4

--- Tutorial/JavatPointWeb/02.Operators/01.02.DoubleParentheses.sh
+++ Tutorial/JavatPointWeb/02.Operators/01.02.DoubleParentheses.sh
@@ -13,4 +13,7 @@ echo "Multiplication:"
 echo -e "\t" $(($Num1 * $Num2)) "\n"
 
 echo "Division:"
-echo -e "\t" $(($Num1 / $Num2))
+echo -e "\t" $(($Num1 / $Num2)) "\n"
+
+echo "Exponentiation:"
+echo -e "\t" $(($Num1 ** $Num2))
