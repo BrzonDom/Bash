3434e7559bb7887907c25e943949d57043d73b33
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 13:29:08 +0200

    Bash 02.Tut v02.5

--- Tutorial/freeCodeCamp/02.02.Variables.sh
+++ Tutorial/freeCodeCamp/02.02.Variables.sh
@@ -1,4 +1,15 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+# Variables
+#   SYNOPSIS:
+#       [variable_name]=[value]
+#   DESCRIPTION:
+#       You can use variables as in any programming languages. There are no 
+#       data types. A variable in bash can contain a number, a character, 
+#       a string of characters.
+
+#       You have no need to declare a variable, just assigning a value to 
+#       its reference will create it.
 
 # cp - copy files and directories
 #   SYNOPSIS:
@@ -7,6 +18,7 @@
 #       cp [OPTION]... -t DIRECTORY SOURCE...
 #   DESCRIPTION:
 #       Copy SOURCE to DEST, or multiple SOURCE(s) to DIRECTORY.
+
 
 LOCATION_FROM=Resources/02.LocFrom
 LOCATION_TO=Resources/02.LocTo
