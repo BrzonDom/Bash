ee283e326428d6966ef112ba1e3dc9371f56deb0
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 13:11:43 +0200

    Bash 10.Tut v02.4

--- Tutorial/freeCodeCamp/10.02.Functions.sh
+++ Tutorial/freeCodeCamp/10.02.Functions.sh
@@ -22,6 +22,26 @@
 #       is loaded all the time while on a 4 CPU system  it  means  it  was
 #       idle 75% of the time.
 
+# Functions
+#   SYNOPSIS:
+#       [function_name] () {
+#           [commands]
+#       }
+#   DESCRIPTION:
+#       Like "real" programming languages, Bash has functions, though in a somewhat 
+#       limited implementation. A function is a subroutine, a code block that 
+#       implements a set of operations, a "black box" that performs a specified task. 
+#       Wherever there is repetitive code, when a task repeats with only slight 
+#       variations in procedure, then consider using a function.
+
+#       The "if COMMANDS" list is executed.  If its exit status is zero, then the
+#       "then COMMANDS" list is executed.  Otherwise, each "elif COMMANDS" list is
+#       executed in turn, and if its exit status is zero, the corresponding
+#       "then COMMANDS" list is executed and the if command completes.  Otherwise,
+#       the "else COMMANDS" list is executed, if present.  The exit status of the
+#       entire construct is the exit status of the last command executed, or zero
+#       if no condition tested true.
+
 
 up="Up time"
 since="Since time"
