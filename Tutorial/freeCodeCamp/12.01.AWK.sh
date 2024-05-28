c23d5c0cf0085e53917d6435858ecb7bc3b96bbe
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 14:54:27 +0200

    Bash 12.Tut v02.2

--- Tutorial/freeCodeCamp/12.01.AWK.sh
+++ Tutorial/freeCodeCamp/12.01.AWK.sh
@@ -1,12 +1,12 @@
- 
+#!/bin/bash 
+
 echo one two three > Resources/12.01.AWKContentTXT.txt
 
 awk '{print $1}' Resources/12.01.AWKContentTXT.txt
-
 awk '{print $2}' Resources/12.01.AWKContentTXT.txt
+
 
 echo one,two,three > Resources/12.01.AWKContentCSV.csv
 
 awk -F, '{print $1}' Resources/12.01.AWKContentCSV.csv
-
 awk -F, '{print $2}' Resources/12.01.AWKContentCSV.csv
