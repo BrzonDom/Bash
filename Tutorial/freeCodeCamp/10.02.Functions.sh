7faf59cd6e7e2398f65409d6e684974c97d12712
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-28 11:19:05 +0200

    Bash 10.Tut v01.0

---
+++ Tutorial/freeCodeCamp/10.02.Functions.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+showuptime() {
+    up=$(uptime -p | cut -c4-)
+    since=$(uptime -s)
+
+    cat << EOF
+    
+----------------------------------
+This machine has been up for ${up}
+It has been running since ${since}
+----------------------------------
+EOF
+}
+
+showuptime
+
+# Execute:
+#   bash 10.02.Functions.sh
