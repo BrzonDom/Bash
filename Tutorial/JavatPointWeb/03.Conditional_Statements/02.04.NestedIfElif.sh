53d6f1d0d028489a63f550625696618088c2a76b
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-29 10:24:36 +0200

    Bash 03.02.Tut 04 v01.1

--- Tutorial/JavatPointWeb/03.Conditional_Statements/02.04.NestedIfElif.sh
+++ Tutorial/JavatPointWeb/03.Conditional_Statements/02.04.NestedIfElif.sh
@@ -4,6 +4,13 @@ if [ $1 -gt 10 ]; then
 
     echo "Number is greater than 10"
 
+    if (( $1 % 2 == 0 )); then
+
+        echo "Number is even"
+
+    else
+        echo "Number is odd"
+
 else
     echo "Number is not greater than 10"
 fi
