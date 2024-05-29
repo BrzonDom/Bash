975485aeb23f57981008cd6330e468b5181eff7e
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-29 06:14:34 +0200

    Bash 03.01.Tut 06 v01.0

---
+++ Tutorial/JavatPointWeb/03.Conditional_Statements/01.06.BasicIf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ 8 -gt 7 ] || [ 10 -eq 3 ];  then
+
+    echo "Condition is True"
+fi
+
+if [ "mylife" == "yourlife" ] || [ 3 -gt 10 ];  then
+
+    echo "Condition is False"
+fi
