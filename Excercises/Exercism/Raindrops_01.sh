d387428416a17261f0272083df10782aa6a3294d
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-29 15:31:18 +0200

    Exercism Raindrops 1 v01.1

--- Excercises/Exercism/Raindrops_01.sh
+++ Excercises/Exercism/Raindrops_01.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ $1 % 3 == 0 ]; then
+if (( $1 % 3 == 0 )); then
 
     echo Pling
 fi
 
-if [ $1 % 5 == 0]; then
+if (( $1 % 5 == 0 )); then
 
     echo Plang
 fi
 
-if [ $1 % 7 == 0]; then
+if (( $1 % 7 == 0 )); then
 
     echo Plong
 fi
