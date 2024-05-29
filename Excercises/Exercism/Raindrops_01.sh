7b013503bb7271b823346813166f03d7cc1f16b4
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-29 15:35:45 +0200

    Exercism Raindrops 1 v01.2

--- Excercises/Exercism/Raindrops_01.sh
+++ Excercises/Exercism/Raindrops_01.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# INTRODUCTION:
+#   Raindrops is a slightly more complex version of the FizzBuzz challenge, a classic 
+#   interview question.
+
+# INSTRUCTION:
+#   Your task is to convert a number into its corresponding raindrop sounds.
+#   If a given number:
+#       is divisible by 3, add "Pling" to the result.
+#       is divisible by 5, add "Plang" to the result.
+#       is divisible by 7, add "Plong" to the result.
+#       is not divisible by 3, 5, or 7, the result should be the number as a string.
+
+# EXAMPLES:
+#   28
+#       => Plong
+#   30
+#       => PlingPlang
+#   34
+#       => 34
+
+
 if (( $1 % 3 == 0 )); then
 
     echo Pling
