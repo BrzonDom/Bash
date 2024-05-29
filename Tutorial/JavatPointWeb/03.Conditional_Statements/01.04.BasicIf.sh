57fd977424891bb850c94eca360bf8ba41d166b9
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-29 03:08:55 +0200

    Bash 03.Tut 04 v01.1

--- Tutorial/JavatPointWeb/03.Conditional_Statements/01.04.BasicIf.sh
+++ Tutorial/JavatPointWeb/03.Conditional_Statements/01.04.BasicIf.sh
@@ -9,3 +9,14 @@ if [ 3 -gt 10 ]; then
 
     echo "3 is not greater than 10"
 fi
+
+
+if [ 3 -lt 10 ]; then
+
+    echo "3 is less than 10"
+fi
+
+if [ 10 -lt 3 ]; then
+
+    echo "10 is not less than 3"
+fi
