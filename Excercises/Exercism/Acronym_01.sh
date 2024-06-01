c181e975f2c33da2adf837d6b1375904a68fee71
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-01 14:42:52 +0200

    Exercism Acronym 1 v02.2

--- Excercises/Exercism/Acronym_01.sh
+++ Excercises/Exercism/Acronym_01.sh
@@ -9,9 +9,20 @@ len=${#Full_Str}
 echo "Input lenght: $len"
 echo
 
+Alph="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 for ((i = 0; i < len; i++)); do
     char="${Full_Str:i:1}"
+    type="None"
+    
+    if echo "$char" | grep "$Alph"; then
+        type="Letter"
+    
+    else
+        type="Non-Letter"
+    fi
 
-    echo -e "  $i.\t$char"
+    echo -e "  $i.\t$char\t$type"
+
 done
  
