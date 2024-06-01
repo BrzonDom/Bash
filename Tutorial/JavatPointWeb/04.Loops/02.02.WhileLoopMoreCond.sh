1dd4885e2d899560f33a1bc75398b6d8f62a729b
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-01 22:37:24 +0200

    Bash 04.02.Tut 02 v02.0

--- Tutorial/JavatPointWeb/04.Loops/02.02.WhileLoopMoreCond.sh
+++ Tutorial/JavatPointWeb/04.Loops/02.02.WhileLoopMoreCond.sh
@@ -3,6 +3,10 @@
 read -p "Enter starting number: " snum  
 read -p "Enter ending number: " enum
 
+echo "Start number: $snum"
+echo "End number:   $enum"
+echo
+
 while [[ $snum -le $enum || $snum == $enum ]]; do
 
     echo $snum  
