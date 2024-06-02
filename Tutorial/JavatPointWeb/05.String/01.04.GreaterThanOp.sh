ac2c154bde6b4dcb6b4bbf31ed88a0a38f889e92
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-03 01:55:36 +0200

    Bash 05.01.Tut 04 v01.0

---
+++ Tutorial/JavatPointWeb/05.String/01.04.GreaterThanOp.sh
@@ -0,0 +1,11 @@
+#!/bin/sh   
+  
+str1="WelcometoJavatpoint"  
+str2="Javatpoint"
+
+if [ $str1 \> $str2 ]; then
+    echo "$str1 is greater then $str2"
+
+else
+    echo "$str1 is not greater then $str2"
+fi
