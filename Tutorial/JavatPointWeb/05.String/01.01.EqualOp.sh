00d107c1badf9ed6d9aab8e68ef030e988eedc06
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-03 01:47:22 +0200

    Bash 05.01.Tut 01 v01.0

---
+++ Tutorial/JavatPointWeb/05.String/01.01.EqualOp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+str1="WelcometoJavatpoint."  
+str2="javatpoint"
+
+if [ $str1 = $str2 ]; then  
+    echo "Both the strings are equal."
+
+else
+    echo "Strings are not equal."  
+fi
