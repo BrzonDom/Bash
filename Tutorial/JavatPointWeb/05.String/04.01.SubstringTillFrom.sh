2ae195adf1d1a33cfbea76026dbee180819ab8c5
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-05 01:30:05 +0200

    Bash 05.04.Tut 01 v01.1

--- Tutorial/JavatPointWeb/05.String/04.01.SubstringTillFrom.sh
+++ Tutorial/JavatPointWeb/05.String/04.01.SubstringTillFrom.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "String: We welcome you on Javatpoint."  
 str="We welcome you on Javatpoint."  
-  
+
+echo "String: $str"
 echo "Total characters in a String: ${#str} "  
-  
+
 substr="${str:0:10}"  
-  
+
 echo "Substring: $substr"  
 echo "Total characters in Substring: ${#substr} "
